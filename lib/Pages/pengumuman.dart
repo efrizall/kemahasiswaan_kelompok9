8b77c41b3c4e26e99adc670242e90e9c8934c432
efrizall <rizal.ef340@gmail.com>
2020-12-04 20:50:07 +0700

    initial commit

--- lib/Pages/pengumuman.dart
+++ lib/Pages/pengumuman.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:kemahasiswaan_kelompok9/Pages/pengumuman-detail.dart';
 
-class Pengumuman extends StatefulWidget{
-  @override
-  _PengumumanState createState() => _PengumumanState();
-}
-
-class _PengumumanState extends State<Pengumuman>{
+class Pengumuman extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        appBar: AppBar(
+          title: Text('Pengumuman'),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text('8 Agustus 2020',
+                  style: TextStyle(height: 2, fontSize: 20)),
+              subtitle: Text('Ormik Mahasiswa Baru',
+                  style: TextStyle(height: 2, fontSize: 25, color: Colors.blue)),
+              trailing: Icon(Icons.arrow_forward, size: 50,),
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => PengumumanDetail()));
+              },
+            ),
+            Divider(
+              color: Colors.black12,
+              height: 20,
+              thickness: 3,
+            ),
+          ],
+        )
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Student App',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
